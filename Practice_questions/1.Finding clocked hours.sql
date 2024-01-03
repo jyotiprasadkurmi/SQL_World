1ef8954d43027adc72255851a5b18f1db068c5da
jyotiprasadkurmi <jyotiprasadk9@gmail.com>
2024-01-03 19:14:42 +0530

    Update 1.Finding clocked hours.sql

--- Practice_questions/1.Finding clocked hours.sql
+++ Practice_questions/1.Finding clocked hours.sql
@@ -12,3 +12,16 @@ insert into clocked_hours values
 (11114,'15:30','O'),
 (11115,'09:30','I'),
 (11115,'17:30','O');
+
+solution1:
+
+with table1 as
+			(select *,
+				lead(swipe,1) over(partition by empd_id order by swipe) as rnk
+			from clocked_hours),
+		table2 as (select *,
+			timestampdiff(hour,swipe,rnk) as clckd_hr
+		from table1
+		where flag = 'I')
+select empd_id, sum(clckd_hr) as clckd from table2
+group by empd_id
